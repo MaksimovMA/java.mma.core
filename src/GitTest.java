b3ccfc603a9df17f5d956bb2b24c2265ea5adb66
Артем Астахов <Astahov_artem@rambler.ru>
2021-05-09 11:27:31 +0300

    change file GitTest

--- src/GitTest.java
+++ src/GitTest.java
@@ -1,9 +1,6 @@
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("HelloGit");
-        System.out.println("HelloGit2");
-        System.out.println("HelloGit3");
-        System.out.println("HelloGit4");
-        System.out.println("HelloGit5");
+
     }
 }
