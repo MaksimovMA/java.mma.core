8abcfd71b39771245c78e77f3adea4b95c57ba0c
Артем Астахов <Astahov_artem@rambler.ru>
2021-05-09 11:07:03 +0300

    Add new file GitTest2 and change file GitTest

--- src/GitTest.java
+++ src/GitTest.java
@@ -3,5 +3,7 @@ public class GitTest {
         System.out.println("HelloGit");
         System.out.println("HelloGit2");
         System.out.println("HelloGit3");
+        System.out.println("HelloGit4");
+        System.out.println("HelloGit5");
     }
 }
